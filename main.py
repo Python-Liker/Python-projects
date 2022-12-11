5bdb46dd3b58bd08b575e58ec68a81d2203ad8f7
Python-Liker <mateiolt2022@gmail.com>
2022-12-11 14:42:09 +0200

    added

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
 #Import libraries
 import random
-import time
 
-
-#Introduction
-print("Press enter to roll the dice")
+#Printing introduction
+print("Hi welcome to number guessing.")
+print("Choose a number between 1 and 100")
+print("Press enter to start")
 input()
 
-#Rolling the dice
-print("Rolling the dice")
-time.sleep(1)
-print(".")
-time.sleep(1)
-print("..")
-time.sleep(1)
-print("...")
-time.sleep(1)
+#Creating the game
+found = False
+n = random.randint(1, 100)
+guesses = 0
 
-#Printing the number on the dice
-n = random.randint(1,6)
-print(f"You rolled {n}")
+while found == False:
+    guess = int(input("Type your guess: "))
+
+    if guess < n and guess >= 1:
+        print("Your guess is lower than the number")
+    elif guess > n and guess <= 100:
+        print("Your guess is greater than the number")
+    elif guess == n:
+        print("You guessed the number correctly")
+        print(f"It took you {guesses} guesses")
+        break
+    elif guess > 100 or guess < 1:
+        print("Please enter a valid number")
